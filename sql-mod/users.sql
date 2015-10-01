abe4f83dc2f3bf79e4802c1611eba06f2ab06fa0
Mark Pat <markpatrick9@gmail.com>
2015-10-01 16:35:30 +0300

    Sql to insert data in the modified tables

--- sql-mod/users.sql
+++ sql-mod/users.sql
@@ -1,1 +1,3 @@
 ALTER TABLE  `care_users` CHANGE  `permission`  `role_id` INT( 10 ) NOT NULL ;
+
+UPDATE  `caredb`.`care_users` SET  `role_id` =  '1' WHERE  `care_users`.`login_id` =  'israel';
---
+++ sql-mod/users.sql~
@@ -0,0 +1,1 @@
+ALTER TABLE  `care_users` CHANGE  `permission`  `role_id` INT( 10 ) NOT NULL ;
